c862c4869d89947f9ef057c49ee7c207bc494bc4
mahmoud alaskalany <mhmoudalaskalany@gmail.com>
2021-02-18 16:33:51 +0400

    fix Task

--- GitBranching/Program.cs
+++ GitBranching/Program.cs
@@ -8,6 +8,7 @@ namespace GitBranching
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Test");
+            Console.WriteLine("Fix Task");
         }
     }
 }
