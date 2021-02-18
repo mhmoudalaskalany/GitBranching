d01676c7d9d3dd13feb84fdb4629c2d7d457ffbc
mahmoud alaskalany <mhmoudalaskalany@gmail.com>
2021-02-18 16:40:06 +0400

    fix startup issue

---
+++ GitBranching/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitBranching
+{
+    public class StartUp
+    {
+        public void Start()
+        {
+            Console.WriteLine("Start Up Commit");
+        }
+    }
+}
